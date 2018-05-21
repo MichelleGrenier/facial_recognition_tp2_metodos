e13a36f7ab3b1481a8754649bab8278dc6529c1a
Franco Pyrih <fpyrih@dc.uba.ar>
2018-05-20 21:32:30 -0300

    que el preprocesador de los tests de la cátedra corra desde donde está (en la misma carpeta que están los tests), sin importar desde dónde lo llamen

--- tests/catedra/preprocesar.sh
+++ tests/catedra/preprocesar.sh
@@ -1,6 +1,8 @@
 
 # agrega "../data/" a cada línea y cambia la extensión .in por .csv
 
+cd $(dirname $0)
+
 for archivo in *.in; do
 
     sed -e 's#^#../data/#' "${archivo}" > `echo preprocesado_$archivo | sed 's/\(.*\.\)in/\1csv/'`
